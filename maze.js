a6b07ff86f99c4318ea8dbf108ae7545eb06bf52
Davia Smith <deedra.dee@hotmail.com>
2018-10-17 15:32:55 -0500

    JS File-Exercise 2

--- maze.js
+++ maze.js
@@ -1,13 +1,31 @@
 let bound1;
+let allBounds;
+let checkWin = false;
+let outOfBounds = false;
 
 window.onload = home;
 
 function home() {
     bound1 = document.getElementById("boundary1");
     bound1.onmouseover = redWall;
+    allBounds = document.querySelectorAll(".boundary");
+    for (let i = 0; i < allBounds.length - 1; i++) {
+		allBounds[i].onmouseover = overWalls;
+    }
 }
 
 function redWall(){
     bound1.className = "youlose";   
 }
 
+function overWalls(){ 
+    if (!checkWin){ 
+        if(!outOfBounds) 
+        {
+			outOfBounds = true;
+			for (let i = 0; i < allBounds.length - 1; i++) {
+			allBounds[i].className += " youlose";
+			}
+		}
+	}
+}
